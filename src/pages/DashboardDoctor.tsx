c3cd37ddfd8be65f14d6f36753c12af257c1ece8
Phan Nhu Trang <jackie@Phans-MacBook-Pro.local>
2025-07-27 16:13:28 +0700

    fix api + auto redirect follow role

--- src/pages/DashboardDoctor.tsx
+++ src/pages/DashboardDoctor.tsx
@@ -1,8 +1,13 @@
+import { useAuthStore } from "./../store/authStore";
+
 export default function DashboardDoctor() {
+  const {logout} = useAuthStore();
+ 
   return (
     <div className="container mt-5">
       <h2>Bảng điều khiển Bác sĩ</h2>
       <p>Chào mừng bạn đến với dashboard của bác sĩ.</p>
+      <button className="btn btn-danger" type="button" onClick={logout}>Logout</button>
     </div>
   );
 }
--- src/pages/DashboardPatient.tsx
+++ src/pages/DashboardPatient.tsx
@@ -1,8 +1,13 @@
+import { useAuthStore } from "./../store/authStore";
+
 export default function DashboardPatient() {
+  const {logout} = useAuthStore();
+
   return (
     <div className="container mt-5">
       <h2>Bảng điều khiển Bệnh nhân</h2>
       <p>Chào mừng bạn đến với dashboard của bệnh nhân.</p>
+      <button className="btn btn-danger" type="button" onClick={logout}>Logout</button>
     </div>
   );
 }
