52ed251cf8923b1322cdb9c352140a31e766ae0d
Jackie <jackie@Jackies-iMac.local>
2025-11-17 17:36:30 +0700

    fix realtime join

--- src/hooks/usePusher.ts
+++ src/hooks/usePusher.ts
@@ -4,10 +4,6 @@ import { PUSHER_CLUSTER, PUSHER_KEY } from "../config/baseConfigs";
 export const createPusherClient = () => {
   return new Pusher(PUSHER_KEY, {
     cluster: PUSHER_CLUSTER, // phải đúng với .env backend
-    // wsHost: "ws-ap1.pusher.com",
-    // wsPort: 443,
-    // wssPort: 443,
     forceTLS: true, // đảm bảo https/wss
-    enabledTransports: ['ws', 'wss'], // ép WebSocket
   });
 };
